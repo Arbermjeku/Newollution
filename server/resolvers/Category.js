e93633538bbaa26fd4b86b1a2419ee916c93ba9d
Arber <arber.mjeku@outlook.com>
2020-04-20 14:17:22 +0200

    updated category

--- server/resolvers/Category.js
+++ server/resolvers/Category.js
@@ -1,13 +1,31 @@
+const { getUserId } = require("../utils");
+
 const addedBy = async (parent, args, context, info) => {
   return await context.prisma.category({ id: parent.id }).addedBy();
 };
 
 const routines = async (parent, args, context, info) => {
-  return await context.prisma.routines({where: { category: { id: parent.id }}})
-}
+  const userId = await getUserId(context);
+  return await context.prisma.routines({
+    where: {
+      category: { id: parent.id },
+      addedBy: { id: userId },
+    },
+  });
+};
+
+const goals = async (parent, args, context, info) => {
+  const userId = getUserId(context);
+  return await context.prisma.goals({
+    where: {
+      category: { id: parent.id },
+      addedBy: { id: userId },
+    },
+  });
+};
 
 module.exports = {
   addedBy,
   routines,
-
+  goals,
 };
